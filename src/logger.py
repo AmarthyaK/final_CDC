bb45abf19172edae987b28b05bd6decd6ce50cd9
AmarthyaK <pragnayamarthyak41@gmail.com>
2023-07-26 19:02:23 -0400

    made changes to requirements.txt

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from datetime import datetime
+import sys
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs",LOG_FILE)
@@ -12,5 +13,10 @@ LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format = "[%(asctime)s] %(lineno)d %(name)s %(levelname)s - %(message)s",
-    level=logging.INFO
+    level=logging.INFO,
+
+    handlers = [
+        logging.FileHandler(LOG_FILE_PATH),
+        logging.StreamHandler(sys.stdout)
+    ]
 )
